6bdf99c1e49404524debc39f5ea7fdfc4d811fbb
DALM1 <dimitri.almon@gmail.com>
2024-04-12 04:15:28 +0200

    Initial Commit

---
+++ SimpleChat/SimpleChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleChatApp.swift
+//  SimpleChat
+//
+//  Created by Dimitri ALMON on 12/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct SimpleChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
